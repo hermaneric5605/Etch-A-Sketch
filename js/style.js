de396a90f4ae02df00b099d0bb8347ee15f772ce
Eric Herman <hermaneric@outlook.com>
2016-09-23 11:27:16 -0400

    added jQuery code and css code

---
+++ js/style.js
@@ -0,0 +1,9 @@
+
+
+$(document).ready(function() {
+    for (var x = 0; x < 16; x++) {
+        for (var y = 0; y < 16; y++) {
+            $("<div>").addClass("unit").appendTo('#container');
+        }
+    }
+});
